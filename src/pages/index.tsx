b47e4cddf0ff5d82808dc63c78f5716238f9c867
unknown <farcwanted222@gmail.com>
2023-08-24 12:00:48 -0300

    adding-links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import { Flex, Heading, Image, chakra, Text, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Image,
+  chakra,
+  Text,
+  Button,
+  keyframes,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import { Projects } from "@/components/Carousel";
+import Link from "next/link";
 
 const Home: React.FC = () => {
+  const colorChange = keyframes`
+  0% { color: #1B70CB; }
+  25% { color: #1B70CB; }
+  50% { color: white; }
+  75% { color: white; }
+  100% { color: #1B70CB; }
+`;
+
   return (
     <>
       <Head>
@@ -30,7 +47,11 @@ const Home: React.FC = () => {
             <Heading fontSize={{ base: 32, md: 60 }} fontWeight={300}>
               Olá, eu sou
             </Heading>
-            <Heading fontSize={{ base: 32, md: 60 }} color="#1B70CB">
+            <Heading
+              fontSize={{ base: 32, md: 60 }}
+              color="#1B70CB"
+              animation={`${colorChange} 5s infinite`}
+            >
               Denner Oliveira<chakra.span color="white">.</chakra.span>
             </Heading>
             <Text maxW={500} fontSize={{ base: 16, md: 18 }} my={5}>
@@ -48,57 +69,73 @@ const Home: React.FC = () => {
               +1.5 anos trabalhando com React e Typescript.
             </Heading>
             <Flex alignItems="center" gap={3} mt={10}>
-              <Button
-                fontWeight="bold"
-                color="black"
-                bg="#1B70CB"
-                rounded="3xl"
-                display="flex"
-                gap={2}
+              <Link href="/dener.cv.pdf" download>
+                <Button
+                  fontWeight="bold"
+                  color="black"
+                  bg="#1B70CB"
+                  rounded="3xl"
+                  display="flex"
+                  gap={2}
+                >
+                  <Image src="/download.svg" w={5} alt="Download" />
+                  Baixar CV
+                </Button>
+              </Link>
+              <Link href="mailto:denerdev013@gmail.com?subject=&body=">
+                <Image
+                  src="/icone_email.svg"
+                  w={7}
+                  alt="Email"
+                  cursor="pointer"
+                  transition=".4s"
+                  _hover={{
+                    w: 8,
+                  }}
+                />
+              </Link>
+              <Link
+                href="https://www.instagram.com/denneroliveira013/"
+                target="_blank"
               >
-                <Image src="/download.svg" w={5} alt="Download" />
-                Baixar CV
-              </Button>
-              <Image
-                src="/icone_email.svg"
-                w={7}
-                alt="Email"
-                cursor="pointer"
-                transition=".4s"
-                _hover={{
-                  w: 8,
-                }}
-              />
-              <Image
-                src="/icone_insta.svg"
-                w={7}
-                alt="Instagram"
-                cursor="pointer"
-                transition=".4s"
-                _hover={{
-                  w: 8,
-                }}
-              />
-              <Image
-                src="/icone_linkedin.svg"
-                w={7}
-                alt="Download"
-                cursor="pointer"
-                transition=".4s"
-                _hover={{
-                  w: 8,
-                }}
-              />
-              <Image
-                src="/icone_whatsapp.svg"
-                w={7}
-                alt="Whatsapp"
-                cursor="pointer"
-                transition=".7s"
-                _hover={{
-                  w: 8,
-                }}
-              />
+                <Image
+                  src="/icone_insta.svg"
+                  w={7}
+                  alt="Instagram"
+                  cursor="pointer"
+                  transition=".4s"
+                  _hover={{
+                    w: 8,
+                  }}
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/dener-oliveira-lima-117496222/"
+                target="_blank"
+              >
+                <Image
+                  src="/icone_linkedin.svg"
+                  w={7}
+                  alt="Download"
+                  cursor="pointer"
+                  transition=".4s"
+                  _hover={{
+                    w: 8,
+                  }}
+                />
+              </Link>
+              <Link href="https://wa.me/5575998468810" target="_blank">
+                <Image
+                  src="/icone_whatsapp.svg"
+                  w={7}
+                  alt="Whatsapp"
+                  cursor="pointer"
+                  transition=".7s"
+                  _hover={{
+                    w: 8,
+                  }}
+                />
+              </Link>
             </Flex>
           </Flex>
           <Image src="/foto_desktop.webp" alt="Dener" />
@@ -110,19 +147,37 @@ const Home: React.FC = () => {
         </Heading>
         <Flex direction="column" mt={20} mb={34}>
           <Flex justifyContent="space-between" w="60%" mx="auto">
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/html5.svg" alt="html5" />
               <Text color="#C9CED3" fontWeight={500}>
                 HTML5
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/css.svg" alt="CSS" />
               <Text color="#C9CED3" fontWeight={500}>
                 CSS3
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image
                 w={100}
                 src="/tecnologias/javascript.svg"
@@ -132,13 +187,25 @@ const Home: React.FC = () => {
                 Javascript
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/react.svg" alt="React" />
               <Text color="#C9CED3" fontWeight={500}>
                 React
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image
                 w={100}
                 src="/tecnologias/typescript.svg"
@@ -150,7 +217,13 @@ const Home: React.FC = () => {
             </Flex>
           </Flex>
           <Flex justifyContent="space-between" w="70%" mx="auto" mt={24}>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image
                 w={100}
                 src="/tecnologias/react-native.svg"
@@ -160,33 +233,61 @@ const Home: React.FC = () => {
                 React Native
               </Text>
             </Flex>
-
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/chakra-ui.svg" alt="Chakra UI" />
               <Text color="#C9CED3" fontWeight={500}>
                 Chakra UI
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/mobx.svg" alt="Mobx" />
               <Text color="#C9CED3" fontWeight={500}>
                 Mobx
               </Text>
             </Flex>
-
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/next.svg" alt="Next" />
               <Text color="#C9CED3" fontWeight={500}>
                 Next
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/node.svg" alt="Node" />
               <Text color="#C9CED3" fontWeight={500}>
                 Node
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/nest_js.svg" alt="Nest" />
               <Text color="#C9CED3" fontWeight={500}>
                 Nest
@@ -194,32 +295,62 @@ const Home: React.FC = () => {
             </Flex>
           </Flex>
           <Flex justifyContent="space-between" w="60%" mx="auto" mt={24}>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/postgres.svg" alt="Postgres" />
               <Text color="#C9CED3" fontWeight={500}>
                 Postgres
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/jest.svg" alt="Jest" />
               <Text color="#C9CED3" fontWeight={500}>
                 Jest
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/typeorm.svg" alt="Typeorm" />
               <Text color="#C9CED3" fontWeight={500}>
                 TypeORM
               </Text>
             </Flex>
 
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/linux.svg" alt="Linux" />
               <Text color="#C9CED3" fontWeight={500}>
                 Linux
               </Text>
             </Flex>
-            <Flex direction="column" alignItems="center" gap={2}>
+            <Flex
+              direction="column"
+              alignItems="center"
+              gap={2}
+              transition=".4s"
+              _hover={{ transform: "scale(1.2)" }}
+            >
               <Image w={100} src="/tecnologias/github.svg" alt="GitHub" />
               <Text color="#C9CED3" fontWeight={500}>
                 GitHub
